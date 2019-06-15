09ab6275302921576218a12abadedf76cdebe62c
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2019-06-15 14:25:17 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { Content1Component } from './content1/content1.component';
+import { Content2Component } from './content2/content2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    SidebarComponent,
+    Content1Component,
+    Content2Component
   ],
   imports: [
     BrowserModule
